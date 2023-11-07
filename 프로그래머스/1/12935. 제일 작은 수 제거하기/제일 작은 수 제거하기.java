81ab4e79d9501740b7b4ec20e25f8b46e98bdc47
조성진 <130537929+Sungjinchosj@users.noreply.github.com>
2023-11-08 00:14:03 +0900

    Update 제일 작은 수 제거하기.java

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,25 +1,38 @@
 class Solution {
-    public int[] solution(int[] arr) {
-        if(arr.length == 1) { // 길이가 1일 경우 answer에 -1 저장
-            int[] answer = {-1};
-            return answer;
-        }
-        
-        int min = arr[0];
-        
-        for(int i = 1; i < arr.length; i++) {
-            min = Math.min(min, arr[i]); // arr배열을 모두 비교 후, 제일 작은 수를 min에 저장
-        }
+   public int[] solution19(int[] arr) {
+                    int[] answer = {};
 
-        int[] answer = new int[arr.length - 1]; // 제일 작은 수를 제외한 모든 arr배열 값을 저장하기 위함
-        int cnt = 0;
-        
-        for(int i = 0; i < arr.length; i++) {
-            if(min == arr[i]) { // 제일 작은 수와 일치한다면 계속, 아니면 answer배열에 담기
-                continue;
-            }
-            answer[cnt++] = arr[i];
-        }        
-    return answer;
-    }
+                    if(arr.length == 1) {
+                    	answer = new int[1];
+                    	
+                    	answer[0] = -1;
+                    }else {
+                    	
+                    	int min = Integer.MAX_VALUE;
+                    	List<Integer> num = new ArrayList<>();
+                    	
+                    	
+                    	for(int i=0; i<arr.length; i++) {
+                    		min = Math.min(min, arr[i]);
+                    	}
+                    	
+                    	for(int i=0; i<arr.length; i++) {
+                    		if(arr[i] != min) {
+                    			num.add(arr[i]);
+                    		}
+                    	}
+                    	answer = new int[num.size()];
+                    	
+                    	for(int i=0; i<answer.length; i++) {
+                    		answer[i] = num.get(i);
+                    		System.out.println(answer[i]);
+                    	}
+                    	
+                    	
+                    	
+                    }
+                    
+                    
+                    return answer;
+                }
 }
