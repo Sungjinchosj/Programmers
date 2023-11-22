cb2a5a93510fd391993988c006487d0f8f742b17
조성진 <130537929+Sungjinchosj@users.noreply.github.com>
2023-11-22 14:32:26 +0900

    Update 명예의 전당 （1）.java

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,39 +1,43 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int k, int[] score) {
-               int[] answer = new int[score.length];
+       int[] answer = new int[score.length];
                     
-                    List<Integer> num = new ArrayList<>();
-                    List<Integer> sNum = new ArrayList<>();
+                    List<Integer> num = new ArrayList<>(); // 명예의 전당 점수 리스트
+                    List<Integer> sNum = new ArrayList<>(); // 최하위 점수 리스트
+                    
                     
                     for(int i=0; i<score.length; i++) {
                     	if(i<k) {
+                    		// 명예의 전당 k일차까지는 모든 점수 다 등록
                     		num.add(score[i]);
                     		num.sort(null);
                     		sNum.add(num.get(0));
                     	}else {
-                    		int min = Integer.MAX_VALUE;
+                    		// k일차 지나고 나서
                     		
-                    		for(int n : num) {
-                    			if(min > n) {
-                    				min = n;
-                    			}
-                    		}
+                    		// 명예의 전당 최하위 점수
+                    		// sort 로 정렬해주었기 때문에 0번 인덱스가 항상 최하위점수
+                    		int min = num.get(0);
+                    		
+                    		// min보다 점수가 클 경우 
                     		if(min <= score[i]) {
+                    			// 0번인덱스(최하위점수) 제거, 새로운 점수 추가
             					num.remove(0);
             					num.add(score[i]);
+            					// 점수별로 정렬
             					num.sort(null);
-            					System.out.println(num);
                     		}
+                    		// 0번 인덱스가 최하위 점수 == 최하위점수 리스트에 추가
                     		sNum.add(num.get(0));
                     	}
                     	
                     	
                     	
                     }
-                    System.out.println(sNum);
                     
+                    
+                    // answer배열에 최하위점수 리스트 넣어주기
                     for(int i=0; i<answer.length; i++) {
                     	answer[i] = sNum.get(i);
                     }
