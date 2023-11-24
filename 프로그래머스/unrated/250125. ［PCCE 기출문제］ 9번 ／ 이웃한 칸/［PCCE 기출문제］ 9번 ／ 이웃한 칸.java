1a58c2c2c86c06021e730b1b5a5028834ab59661
조성진 <130537929+Sungjinchosj@users.noreply.github.com>
2023-11-24 22:07:09 +0900

    Update ［PCCE 기출문제］ 9번 ／ 이웃한 칸.java

--- 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -2,18 +2,30 @@ class Solution {
     public int solution(String[][] board, int h, int w) {
         int answer = 0;
         
+        // 지정색 저장
         String color = board[h][w];
 
+        
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[i].length; j++){
+                // board[i][j] == j가 w인 경우 
                 if(j == w){
+                    // 위아래 색을 알아야한다.
+                    // board[i][w] 에서 위아래를 확인할 수 있도록
+                    // i에서 1을 빼면 바로 위, 더하면 아래를 확인할 수 있다.
                      if(i == h-1 || i == h+1){
+                         // 지정색이랑 일치하면 카운트 증가
                          if(color.equals(board[i][j])){
                              answer++;
                          }
                      }
-                }else if(i == h){
+                }else if(i == h){ // 양 옆 색을 알아야 한다.
+                    // board[i][j] == i가 h인 경우
+
+                    // board[h][j] 에서 양 옆을 확인할 수 있도록
+                    // j에서 1을 빼면 왼쪽, 더하면 오른쪽을 확인할 수 있다.
                     if(j == w-1 || j == w+1){
+                        // 지정색이랑 일치하면 카운트 증가
                          if(color.equals(board[i][j])){
                              answer++;
                          }
@@ -21,7 +33,7 @@ class Solution {
                 }
                
             }
-            System.out.println();
+       
         }
         
         return answer;
